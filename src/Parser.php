977aa7ee276ef10ca543d06f97ff043e98d4c505
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-07-26 23:23:56 +0500

    refactor(main): create parser

--- src/Parser.php
+++ src/Parser.php
@@ -2,19 +2,36 @@
 
 declare(strict_types=1);
 
-namespace VendorName\Skeleton;
+namespace FreeId\Sqlite;
 
+use FreeId\Core\Concerns\Database;
+use FreeId\Contracts\SqliteDatabase as SqliteDatabaseContract;
 use FreeId\Core\Parser as BaseParser;
 
-class Parser extends BaseParser
+class Parser extends BaseParser implements SqliteDatabaseContract
 {
-    public function __construct() {
-        // Constructor code ...
+    use Database;
+
+    public function __construct(
+        string $path,
+        string $table,
+        string $column = 'id',
+        int $start_id = 1,
+    ) {
+        parent::__construct([], $start_id);
+        $this->path = $path;
+        $this->table = $table;
+        $this->column = $column;
     }
 
     public function find(): int
     {
-        // Parser code ...
+        $this->data = $this->getData(
+            'sqlite:' . $this->path,
+            ['username' => null, 'password' => null],
+            $this->table,
+            $this->column,
+        );
 
         return $this->enumerate();
     }
